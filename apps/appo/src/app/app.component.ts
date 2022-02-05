303871187726dc67956d6c02c114956e7c313513
Mike Haas <mikehaas763@gmail.com>
2022-02-05 16:30:54 -0500

    initial commit

---
+++ apps/appo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'testsharedstyles-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'appo';
+}
